149d03c3869c83f19fcc7258c19a281e744bf7f2
Chasoso <hideaki.yamamoto23@gmail.com>
2026-03-11 20:27:30 +0900

    fix a bug in test flow

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                dashboard: resolve(__dirname, "dashboard.html")
+                dashboard: resolve(__dirname, "dashboard/index.html")
             }
         }
     }
